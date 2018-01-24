9224fd166e3807ce53cbfc9d1708c38a47b85354
radishpower <grace@pixels.io>
2018-01-24 08:05:00 -0800

    Added new hw02 files

---
+++ ccsf/cs111a_sp18/HelloWorldApp.java
@@ -0,0 +1,27 @@
+/* Craig Persiko
+   HelloWorldApp.java
+   Sample program to demonstrate JOptionPane
+   input and output, and console output.
+
+   NOTE: This GUI i/o will not work on hills.
+*/
+
+import javax.swing.JOptionPane;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        String response;
+        int age;
+
+        JOptionPane.showMessageDialog(null, "Hello World!");
+        response = JOptionPane.showInputDialog("Enter your age");
+        age = Integer.parseInt(response);
+        System.out.println("In 2 years, you'll be: " + (age + 2));
+        // parentheses are needed above to add age + 2
+        // before adding to string (try without inner parentheses)
+
+        System.exit(0);  // needed to end program when GUI used
+    }
+}
+
+
